f4ac6b98acf63558f9984ad13d79dc5c7e6da7d4
Andrea <97256300+xatisba@users.noreply.github.com>
2024-08-10 15:41:50 +0200

    App.py update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,14 @@
-from utils import db_connect
-engine = db_connect()
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# your code here
+import seaborn as sns # Visualization
+import matplotlib.pyplot as plt # Visualization
+from colorama import Fore
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import math
+
+import warnings # Supress warnings 
+warnings.filterwarnings('ignore')
+
+np.random.seed(7)
